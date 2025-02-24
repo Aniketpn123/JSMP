5f211723fb5b0c829f421ca4cdf1f05a5793302d
ANIKET <aniketpn123@gmail.com>
2025-02-25 00:27:00 +0530

    todo

---
+++ toDoList/script.js
@@ -0,0 +1,17 @@
+ const inputBox=document.getElementById(input-box);
+ const listContainer=document.getElementById(list-container);
+
+
+ function addTask(){
+   if(inputBox.value === '')
+   {
+      alert("write something");
+   }
+   else{
+    let li=document.createElement("li");
+    li.innerHTML=inputBox.value;
+    listContainer.appendChild(li);
+   
+   }
+   inputBox.vlaue="";
+ }
