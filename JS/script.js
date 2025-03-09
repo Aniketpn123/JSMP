3bf0f1f8b31bde6656a9482a40e688ae1a8edf97
ANIKET <aniketpn123@gmail.com>
2025-03-09 11:56:07 +0530

    ANiekt

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+// let a=9;
+//     console.log(window.document);
+//     console.log(a);
+//     console.log(document.head.innerText);
+//     console.log(document.body.innerHTML);
+//     console.log(document.body.innerHTML);
+
+//     console.log(document.links[0]);
+    
+//     document.body.innerHTML='<h1>hello</h1>';
+
+//     document.write('hello');
+    
+    console.log(document.getElementById('main'));
+    
+    let main=document.getElementById('main');
+    // main.innerHTML='<h1>hdhdh</h1>';
+    document.querySelector('#main').innerText='hello';
---
+++ JS/store/script.js
@@ -0,0 +1,28 @@
+let output;
+output=document.all;
+output=document.all[11];
+output=document.all.length;
+
+
+output=document.documentElement;
+
+output= document.head;
+output= document.body;
+output= document.body.children;
+
+output=document.doctype;
+output=document.domain;
+output=document.URL;
+output=document.characterSet;
+output=document.contentType;
+
+output=document.forms[0];
+output=document.forms[0].id;
+output=document.forms[0].method;
+output=document.forms[0].action;
+
+// document.forms[0].id='new-id';
+
+
+
+console.log(output);
